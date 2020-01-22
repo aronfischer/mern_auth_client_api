988fdf1f3b3136d129d43c5e9430b666c03baa43
Aron Fischer <aronfischersigl@googlemail.com>
2020-01-22 08:47:19 +0100

    deployment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./core/Layout";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Layout>
+      <div className='col-md-6 mx-auto mt-5 text-center '>
+        <h1 className='mb-5'>
+          React Node MongoDB Atlas Authentication Boilerplate
+        </h1>
+        <h2>MERN STACK</h2>
+        <hr />
+        <p className='lead'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta,
+          labore magni exercitationem veritatis voluptatibus molestias, minus
+          ratione numquam atque quos nemo optio repellendus voluptatum tempore,
+          nobis natus hic tempora non? Odit dolorum ratione accusamus, nemo,
+          optio animi quia, illum veritatis cupiditate vitae deserunt tempora
+          autem doloribus! Officia ratione itaque quidem, perferendis quod porro
+          suscipit minima, dolores magni eum minus eaque! Et aliquid aut
+          molestias! Facilis pariatur nostrum, similique blanditiis vero veniam
+          aliquam non et, itaque eveniet recusandae tempore deleniti delectus ea
+          laudantium dolor quos veritatis dolore voluptate nulla sunt? Alias?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </Layout>
   );
-}
+};
 
 export default App;
